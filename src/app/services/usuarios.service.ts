f81ab59c4fd0b3c1eea32e39868c91135221cd9d
Marcos Quispe <jawirana@hotmail.com>
2021-06-13 20:23:25 -0300

    CSS update

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
 
 export class UsuariosService{
 	API_URI = 'http://localhost:3000/user';
+	// API_URI = 'https://ppiii-demo1.herokuapp.com';
 	
 	constructor(private http: HttpClient, private router:Router) { }
 
